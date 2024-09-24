0d6c43ddedeb11402d4f24dac755ab8f5ce9a673
FelipeAT <pipeaviles1608@gmail.com>
2024-09-23 23:09:05 -0500

    Se agrego controlador de tiempos para memorizar y se rediseñaron los indicadores y las cartas de dificultad

---
+++ youNeverWalkAlone/Partida/Categoria_Numeros/assets/js/Memorizacion Dificultad/mainMemorizacionFacil.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+    iniciarBarraProgreso('#barraProgresoFacil', 8000, 'Memorizacion Medio.html');  
+})
---
+++ youNeverWalkAlone/Partida/Categoria_Numeros/assets/js/Memorizacion Dificultad/mainMemorizacionMedia.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+    iniciarBarraProgreso('#barraProgresoMedia', 10000, 'Memorizacion Dificil.html'); 
+})
